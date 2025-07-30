13c199128d53a2cf0358aa5b68546657b28d1c25
ZynthDev <zynthapp8@gmail.com>
2025-07-30 03:05:03 -0700

    Optimise Docker Image

--- app/services/pdf_generator.py
+++ app/services/pdf_generator.py
@@ -4,7 +4,8 @@ from io import BytesIO
 from jinja2 import Environment, FileSystemLoader
 import os
 
-env = Environment(loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), '..', 'templates')))
+TEMPLATE_DIR = os.path.join(os.getcwd(), "app", "templates")
+env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
 template = env.get_template("ovlo_template.html")
 
 async def generate_pdf_from_html(file):
